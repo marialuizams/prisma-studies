6bee9a50f84fb4c59d222fa733c2ac339dce0824
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-06-12 18:45:33 -0300

    feat: list courses with pagination

---
+++ src/courses/pagination.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  let skip = 0;
+  let exists = true;
+
+  while (exists) {
+    const result = await prisma.courses.findMany({
+      skip: skip,
+      take: 2
+    });
+
+    skip += 2;
+
+    if (result.length <= 0) {
+      exists = false;
+    }
+
+    if (exists) {
+      console.log(result);
+      console.log("--------------");
+    }
+  }
+}
+
+main();
