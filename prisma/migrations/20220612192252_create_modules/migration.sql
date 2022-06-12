4eb50d3cd4c89b7978a253159191b0fbcd0fb691
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-06-12 16:25:18 -0300

    refactor: change modules model name

--- prisma/migrations/20220612192252_create_modules/migration.sql
+++ prisma/migrations/20220612192252_create_modules/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "Modules" (
+CREATE TABLE "modules" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Modules_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "modules_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Modules_name_key" ON "Modules"("name");
+CREATE UNIQUE INDEX "modules_name_key" ON "modules"("name");
